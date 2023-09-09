1f5bb103bd5dccc0e8dcec8b300d05406f9ec3d4
Lawen-s <bg920611@gmail.com>
2023-09-09 17:58:52 +0900

    board 폴더 생성

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BoardsService {
+    private boards = [];
+
+
+
+    getAllboards(){
+        return this.boards;
+    }
+}
---
+++ src/common/http_exception.filter.ts
@@ -0,0 +1,7 @@
+import { Catch, ExceptionFilter } from "@nestjs/common";
+
+
+// @Catch()
+// export class HttpExceptionFilter implements ExceptionFilter {
+//     private readonly logger: 
+// }
