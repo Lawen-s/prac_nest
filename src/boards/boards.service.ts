ecf4ce159023ff10a5a1f6fb1bd7fd012a8a4711
Lawen-s <bg920611@gmail.com>
2023-09-18 23:08:38 +0900

    Feat: board update

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Board, boardStatus } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import {v1 as uuid} from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
 export class BoardsService {
     private boards: Board[] = [];
@@ -11,14 +12,30 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        const {title, description} = createBoardDto
         const board: Board = {
             id:uuid(),
             title,
             description,
-            status: boardStatus.PRIVATE
+            status: BoardStatus.PRIVATE
         }
         this.boards.push(board);
+        return board;
+    }
+
+    getBoardById(id: string): Board {
+        return this.boards.find(board => board.id === id)
+    }
+
+    delete(id:string): void {
+        this.boards = this.boards.filter(board => board.id !== id)
+        return 
+    }
+
+    update(id:string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
         return board;
     }
 }
