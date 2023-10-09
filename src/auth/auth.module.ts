a9b44d6081f4caaf207c7b23d3e23df3b5032173
Lawen-s <bg920611@gmail.com>
2023-10-09 23:49:10 +0900

    fix: auth.service delete

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './interface/auth.controller';
-import { AuthService } from './auth.service';
 import { CqrsModule } from '@nestjs/cqrs';
 import { UserRepositoryImplement } from './infrastructure/repository/user.repository-implement';
 import { UserSignUpHandler } from './application/command/sign-up/sign-up.handler';
