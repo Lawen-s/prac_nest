31d9eecfa5bbfa325f781f48f4803aad5e3693d4
Lawen-s <bg920611@gmail.com>
2023-10-25 00:09:41 +0900

    Feat: bcrypt install

--- src/auth/application/command/sign-up/sign-up.handler.ts
+++ src/auth/application/command/sign-up/sign-up.handler.ts
@@ -1,11 +1,10 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { UserSignUpCommand } from "./sign-up.command";
 import { UserRepository } from "../user.repository";
-import { UserRepositoryImplement } from "src/auth/infrastructure/repository/user.repository-implement";
 import { Inject, NotFoundException } from "@nestjs/common";
 import { User } from "src/auth/infrastructure/entity/user.entity";
 import { InjectionToken } from "../../inject-token";
-
+import * as bcrypt from 'bcryptjs';
 
 
 @CommandHandler(UserSignUpCommand)
@@ -17,8 +16,12 @@ export class UserSignUpHandler implements ICommandHandler<UserSignUpCommand> {
     // if (existUser) {
     //   throw new NotFoundException(`Can't use username ${command.username}` )
     // }
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(command.password,salt);
+
     const emptyUser = new User();
     Object.assign(emptyUser,command)
+    emptyUser.password = hashedPassword;
     await this.userRepository.save(emptyUser)
   }
 }
